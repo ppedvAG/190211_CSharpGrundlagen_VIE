41d0ab650bde9c92578571831c5f0acdf9deae97
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-14 14:35:28 +0100

    SQLAbfrage

--- SQLAbfrage_Demo/SQLAbfrage_Demo/Program.cs
+++ SQLAbfrage_Demo/SQLAbfrage_Demo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace SQLAbfrage_Demo
     {
         static void Main(string[] args)
         {
+            SqlConnection con = new SqlConnection("Data Source=(localDB)\\MSSQLLocalDB");
+            con.Open();
+
+            SqlCommand meinCommand = new SqlCommand($"SELECT * FROM Northwind.Dbo.Products",con);
+
+            SqlDataReader reader = meinCommand.ExecuteReader();
+
+            while(reader.Read())
+            {
+                int id = (int)reader["ProductID"];
+                Console.WriteLine(id);
+            }
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
