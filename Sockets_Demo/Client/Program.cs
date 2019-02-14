664cdf5d3c106217a527e6fe9d64d46787b95bf6
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-14 14:51:51 +0100

    Socket-Demo mit UDP

--- Sockets_Demo/Client/Program.cs
+++ Sockets_Demo/Client/Program.cs
@@ -6,17 +6,24 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sockets_Demo
+namespace Client
 {
     class Program
     {
         static void Main(string[] args)
         {
             Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            s.Bind(new IPEndPoint(IPAddress.Loopback, 9999));
 
-            byte[] uhrzeit = Encoding.Default.GetBytes(DateTime.Now.ToLongTimeString());
-            s.SendTo(uhrzeit, new IPEndPoint(IPAddress.Loopback, 9999));
+            // warten:
 
+            while (true) // Endlosschleife
+            {
+                byte[] buffer = new byte[1024];
+                int totalBytes = s.Receive(buffer, 1024, SocketFlags.None);
+                string message = Encoding.Default.GetString(buffer, 0, totalBytes);
+                Console.WriteLine(message);
+            }
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
